442288b006504e87bb15f32a16dd19d83eaece9a
shailesh097 <sailesh.pokharel.234@gmail.com>
2022-12-22 14:48:07 +0545

    modified:   Readme.md 	new file:   test.py 	modified:   tools.py

---
+++ test.py
@@ -0,0 +1,4 @@
+def is_palindrome(varchar: str) -> bool:
+	print(type(varchar))
+
+is_palindrome('hello')
--- tools.py
+++ tools.py
@@ -1,20 +1,17 @@
+import math
 
-def reverse_a_number(number):
+def reverse_a_number(number: int) -> int:
 	"""This function takes a number and reverses it.
-
 	Args:
 		number (int): Enter the number to reverse.
-
 	Returns:
 		int: Number after it is reversed.
 	"""
-	reverse = 0
-	while n!=0:
-		reverse = reverse*10 + n%10
-		n = (n//10)
-	return reverse
+	str_number = str(number)
+	reverse = str_number[::-1]
+	return int(reverse)
 
-def is_armstrong(number):
+def is_armstrong(number: int) -> bool:
 	"""This function checks if a number is armstrong or not.
 
 	Args:
@@ -33,3 +30,69 @@ def is_armstrong(number):
 	
 	return sum_digits == number
 
+def is_prime(number: int) -> bool:
+	"""Check if the "number" is prime or not.
+	Args:
+		number (int): The number to be checked.
+	Returns:
+		True: If the number is prime.
+		False: If the number is not prime. 
+	"""
+	if number < 2:
+		return False
+	
+	for i in range(2, int(math.sqrt(number)) + 1):
+		if number % i == 0:
+			return False
+
+	return True
+
+def n_fibonacci(number: int) -> list:
+	"""Returns the list of "number" of fibonacci series items starting from 0.
+	Args:
+		number (int): Number of fibonacci number to return in the list. (number>0)
+	Returns:
+		list: n number of fibonacci number.
+	"""
+	a, b = 0, 1
+	result = []
+	while len(result)<n:
+		result.append(n)
+		a, b = b, a+b
+	return result
+
+def is_palindrome(var) -> bool:
+	"""Checks if the given input is palindorme or not.
+	Args:
+		var (int/str): Input to check for palindorme.
+	Raises:
+		Exception: Can check only for string and integer type.
+	Returns:
+		True: If the given input is palindrome.
+		False: If the given input is not palindrome.
+	"""
+	if type(var) == 'str':
+		reverse = var[::-1]
+	elif type(var) == 'int':
+		to_str = str(var)
+		reverse = to_str[::-1]
+	else:
+		raise Exception("Can only test for string and integer. Try changing the input to either integer or string.")
+	
+	return var == reverse
+
+def is_binary(number: int) -> bool:
+	"""Checks if the given input number is binary or not.
+	Args:
+		var (int): Number to check.	
+	Returns:
+		True: If the number is binary.
+		False: If the number is not binary.
+	"""
+	to_string = str(var)
+	
+	for each_number in string:
+		if each_number != '0' and each_number != '1':
+			return False
+
+	return True
